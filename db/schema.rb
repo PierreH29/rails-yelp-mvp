787aff747ee760486398c240834693ac207f602e
Pierre Hamon <pierrehamon@gmail.com>
2018-11-21 12:24:44 +0100

    add review modell

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_21_112024) do
+ActiveRecord::Schema.define(version: 2018_11_21_112414) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.text "address"
     t.integer "phone_number"
     t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

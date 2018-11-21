a6a38e9b97c667e2b45ef1bb88e416373767491f
Pierre Hamon <pierrehamon@gmail.com>
2018-11-21 14:30:29 +0100

    Rails new with frontend and form gems

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,17 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews # => dishoom.reviews
+  validates :name, :address, presence:true
+  validates :category, inclusion: { in: %w("chinese", "italian", "japanese", "french", "belgian")}, presence: true
+
+  after_destroy do
+        unless @restaurant.reviews.any?
+            @restaurant.destroy
+        end
+      end
+
+  # scope :top, -> { where(stars: 5) }
+
+  # def self.top
+  #   where(stars: 5)
+  # end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,6 @@
 class Review < ApplicationRecord
+  belongs_to :restaurant
+  # => review1.restaurant, Review.create(restaurant: dishoom)
+  validates :content, presence:true
+  validates :rating, length: { in: 0..5 }, presence:true
 end
